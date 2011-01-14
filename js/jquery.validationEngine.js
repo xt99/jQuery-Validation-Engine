e027d912a928d28c9cadad35b15783cf4a47ced2
Cedric Dugas <cedric_dugas@hotmail.com>
2011-01-13 21:12:38 -0500

    change revert

--- js/jquery.validationEngine.js
+++ js/jquery.validationEngine.js
@@ -1093,14 +1093,10 @@
      * @param {String}
      *            method (optional) action
      */
-    $.fn.validationEngine = function(method, action) {
+    $.fn.validationEngine = function(method) {
 
         var form = $(this);
-        // skip private methods : _samplePrivateMethod
-        if(action && typeof(method) === 'string' && method.charAt(0) != '_' && methods[method]) {
-            // make sure init is being called at least once
-            methods.init.apply(form);
-        }
+
         if (typeof(method) === 'string' && method.charAt(0) != '_' && methods[method]) {
             // make sure init is being called at least once
             methods.init.apply(form);
